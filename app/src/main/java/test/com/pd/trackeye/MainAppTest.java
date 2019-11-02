f154c201dfe7a2302e402058a8a7c24c69339d81
Mohammed Ottun <ottunmo@mac.com>
2019-11-02 11:38:00 -0500

    Revert "Revert "Create test classes""
    This reverts commit 9cf38845574de60cbe40c5034c7c71e82ef2f330.

---
+++ app/src/main/java/test/com/pd/trackeye/MainAppTest.java
@@ -0,0 +1,355 @@
+package test.com.pd.trackeye; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* MainApp Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Nov 2, 2019</pre> 
+* @version 1.0 
+*/ 
+public class MainAppTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: onCreate(Bundle savedInstanceState) 
+* 
+*/ 
+@Test
+public void testOnCreate() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: playPing() 
+* 
+*/ 
+@Test
+public void testPlayPing() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: playAlarm() 
+* 
+*/ 
+@Test
+public void testPlayAlarm() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: pauseAlarm() 
+* 
+*/ 
+@Test
+public void testPauseAlarm() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: createCameraSource() 
+* 
+*/ 
+@Test
+public void testCreateCameraSource() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onResume() 
+* 
+*/ 
+@Test
+public void testOnResume() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onPause() 
+* 
+*/ 
+@Test
+public void testOnPause() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onDestroy() 
+* 
+*/ 
+@Test
+public void testOnDestroy() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: showStatus(final String message) 
+* 
+*/ 
+@Test
+public void testShowStatus() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onUpdate(Detector.Detections<Face> detections, Face face) 
+* 
+*/ 
+@Test
+public void testOnUpdate() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: EyesClosed(Detector.Detections<Face> detections, Face face, float threshold) 
+* 
+*/ 
+@Test
+public void testEyesClosed() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: HeadTurnedLeft(Detector.Detections<Face> detections, Face face, float threshold) 
+* 
+*/ 
+@Test
+public void testHeadTurnedLeft() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: HeadTurnedRight(Detector.Detections<Face> detections, Face face, float threshold) 
+* 
+*/ 
+@Test
+public void testHeadTurnedRight() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onMissing(Detector.Detections<Face> detections) 
+* 
+*/ 
+@Test
+public void testOnMissing() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onDone() 
+* 
+*/ 
+@Test
+public void testOnDone() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: create(Face face) 
+* 
+*/ 
+@Test
+public void testCreate() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onDump(String prefix, FileDescriptor fd, PrintWriter writer, String[] args) 
+* 
+*/ 
+@Test
+public void testOnDump() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onShouldSaveFragmentState(Fragment fragment) 
+* 
+*/ 
+@Test
+public void testOnShouldSaveFragmentState() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onGetLayoutInflater() 
+* 
+*/ 
+@Test
+public void testOnGetLayoutInflater() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onGetHost() 
+* 
+*/ 
+@Test
+public void testOnGetHost() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onSupportInvalidateOptionsMenu() 
+* 
+*/ 
+@Test
+public void testOnSupportInvalidateOptionsMenu() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onStartActivityFromFragment(Fragment fragment, Intent intent, int requestCode) 
+* 
+*/ 
+@Test
+public void testOnStartActivityFromFragmentForFragmentIntentRequestCode() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onStartActivityFromFragment(Fragment fragment, Intent intent, int requestCode, @Nullable Bundle options) 
+* 
+*/ 
+@Test
+public void testOnStartActivityFromFragmentForFragmentIntentRequestCodeOptions() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onStartIntentSenderFromFragment(Fragment fragment, IntentSender intent, int requestCode, @Nullable Intent fillInIntent, int flagsMask, int flagsValues, int extraFlags, Bundle options) 
+* 
+*/ 
+@Test
+public void testOnStartIntentSenderFromFragment() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onRequestPermissionsFromFragment(@NonNull Fragment fragment, @NonNull String[] permissions, int requestCode) 
+* 
+*/ 
+@Test
+public void testOnRequestPermissionsFromFragment() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onShouldShowRequestPermissionRationale(@NonNull String permission) 
+* 
+*/ 
+@Test
+public void testOnShouldShowRequestPermissionRationale() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onHasWindowAnimations() 
+* 
+*/ 
+@Test
+public void testOnHasWindowAnimations() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onGetWindowAnimations() 
+* 
+*/ 
+@Test
+public void testOnGetWindowAnimations() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onAttachFragment(Fragment fragment) 
+* 
+*/ 
+@Test
+public void testOnAttachFragment() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onFindViewById(int id) 
+* 
+*/ 
+@Test
+public void testOnFindViewById() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onHasView() 
+* 
+*/ 
+@Test
+public void testOnHasView() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: closeApplication() 
+* 
+*/ 
+@Test
+public void testCloseApplication() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = MainApp.getClass().getMethod("closeApplication"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
